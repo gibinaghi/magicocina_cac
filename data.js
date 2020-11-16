83e4508006ad383ec40eedb5bbb7c36984247ab4
Gimena <gimenabinaghi@hotmail.com>
2020-11-16 14:19:43 -0300

    first commit

---
+++ data.js
@@ -0,0 +1,21 @@
+var data=[
+    {
+        codigo: "cod1",
+        imagen: "img/desayuno.png",
+        nombre: "Desayunos",
+        descripcion: "Las más geniales ideas de desayunos"
+    },
+    {
+        codigo: "cod2",
+        imagen: "img/ensalada.png",
+        nombre: "Ensaladas",
+        descripcion: "Las rectas de ensaladas saludables"
+    },
+    {
+        codigo: "cod3",
+        imagen: "img/panes.png",
+        nombre: "Panes",
+        descripcion: "Las mejores recetas de panes"
+    }
+
+]
---
+++ mypage.js
@@ -0,0 +1,15 @@
+function formRedirect(){
+    window.location.href= "form.html";
+}
+
+function homeRedirect(){
+    window.location.href= "index.html";
+}
+
+function aboutUsRedirect(){
+    window.location.href= "aboutus.html";
+}
+
+function serviceRedirect(){
+    window.location.href= "service.html";
+}
